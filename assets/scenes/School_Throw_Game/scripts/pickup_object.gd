517c6f57d82c086d826ce51c308eae8030caf576
leoshengsss <shenleon66jm@gmail.com>
2026-05-30 14:30:30 -0700

    this should get rid of the infinite loop
    gets rid of inf loop

--- assets/scenes/School_Throw_Game/scripts/pickup_object.gd
+++
@@ -1,8 +0,0 @@
-extends RigidBody3D
-
-func get_interaction_text() -> String:
-	return get_meta("interaction_text", "Press E: Pick up")
-
-func interact(player: Node) -> void:
-	if player.has_method("pickup"):
-		player.call("pickup", self)
