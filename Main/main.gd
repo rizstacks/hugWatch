26d7d2dd3984ebb7216ca607eb64cf41ec0e9b6e
rizbunz <itellezh@ucsc.edu>
2026-06-03 01:46:12 -0700

    game crashes
    games crashes

--- Main/main.gd
+++ Main/main.gd
@@ -1,6 +1,17 @@
 extends Node3D
 
 @export var map_only: bool = true
+@export var npcCount = 10
 
-func _ready() -> void:
-	pass
+var npcScene = preload("res://Spawnables/npc.tscn")
+
+func _ready():
+	var npcs = []
+	for i in range(npcCount):
+		var npc = npcScene.instantiate()
+		add_child(npc)
+		npc.global_position = Vector3(randf_range(-10, 10), 1, randf_range(-10, 10))
+		npcs.append(npc)
+		if i%2 == 1:
+			npcs[i].pair = npcs[i-1]
+			npcs[i-1].pair = npcs[i]
