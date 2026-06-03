49d776c6c1bbb3feabdb7e1e88273c387235b40b
rizbunz <itellezh@ucsc.edu>
2026-06-03 01:20:07 -0700

    idk what this is riz

--- Main/main_menu.gd
+++ Main/main_menu.gd
@@ -5,17 +5,17 @@ extends Control
 @onready var quit_button = $MarginContainer/VBoxContainer/QuitButton
 
 func _ready():
-    play_button.pressed.connect(_on_play_pressed)
-    options_button.pressed.connect(_on_options_pressed)
-    quit_button.pressed.connect(_on_quit_pressed)
-    
-    play_button.grab_focus()
+	play_button.pressed.connect(_on_play_pressed)
+	options_button.pressed.connect(_on_options_pressed)
+	quit_button.pressed.connect(_on_quit_pressed)
+	
+	play_button.grab_focus()
 
 func _on_play_pressed():
-    get_tree().change_scene_to_file("res://Main/main.tscn")
+	get_tree().change_scene_to_file("res://Main/main.tscn")
 
 func _on_options_pressed():
-    print("Options menu opening...")
+	print("Options menu opening...")
 
 func _on_quit_pressed():
-    get_tree().quit()
+	get_tree().quit()
