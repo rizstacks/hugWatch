87a2155ba93dd18183fadeaa1b63bdab752ed18c
belli-design <belli@ucsc.edu>
2026-06-11 11:33:45 -0700

    Update main_menu.gd

--- Main/main_menu.gd
+++ Main/main_menu.gd
@@ -14,7 +14,7 @@ func _ready() -> void:
 
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file(res://Main/main.tscn)
+	get_tree().change_scene_to_file("res://Main/main.tscn")
 
 
 func _on_quit_pressed() -> void:
